2ed81009c13c1f775179bd761aa81d68fbba5b0b
sahiljain112 <sahiljain112@gmail.com>
2016-11-05 17:19:26 +0530

    Basic server

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var sensors = require('./routes/sensors');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/query', sensors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
