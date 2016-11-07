e401e8eb8c8054ded3cc07322eb63cfe84beac7c
sahiljain112 <sahiljain112@gmail.com>
2016-11-07 16:25:09 +0530

    Helper and Message ulility update

---
+++ public/javascripts/helper.js
@@ -0,0 +1,56 @@
+/* This is the file which reads in all the sensor data.
+
+1) It has methods which can return sensor reading at any time.
+2) Every 5 mins, the average reading of the sensor is sent and UI is updated.
+
+*/
+
+var express = require('express');
+var credentials = require('./token.js');
+var mraa = require('mraa'); //require mraa
+
+console.log('MRAA Version: ' + mraa.getVersion());
+
+var tempPin = new mraa.Aio(0); //setup access analog input Analog pin #0 for temperature sensing!
+var buzzerPin = new mraa.Gpio(12);
+var proximity = new mraa.Gpio(0);
+var stepCount = 0, avgTemp = 0, avgProximity = 0;     // Measured as 0,1!
+
+var utility = {
+
+  getTemperature: function(){
+    var analogValue = tempPin.read() * 0.004882814;
+    return ((analogValue - 0.5) * 100);
+  },
+
+  getProximity: function() {
+    var proximityValue = proximity.read();
+    if(proximityValue === 1)
+       buzzerPin.write(1);
+    return proximityValue;
+  },
+
+  getAggregatedProximity: function() {
+    return avgProximity/stepCount;
+  },
+
+  getAggregatedTemperature: function() {
+    return avgTemp/stepCount;
+  },
+
+  setAggregatedTemperature: function () {
+    avgTemp = avgTemp + getTemperature();
+  },
+
+  setAverageProximity: function() {
+    avgProximity = aveProximity + getProximity();
+    stepCount = stepCount + 1;
+  },
+
+  resetValues: function() {
+    avgProximity = avgTemp = stepCount = 0;
+  }
+
+}
+
+module.exports = utility;
--- routes/sensors.js
+++ routes/sensors.js
@@ -1,12 +1,61 @@
 var express = require('express');
+var sendMessage = require('../public/javascripts/message');
+var utility = require('../public/javascripts/helper');
 var router = express.Router();
 
+var jsonResponse = {
+  tempValue: 0,
+  proximityValue: 0,
+  timeStamp: new Date().getTime(),
+  tempState: 'SAFE',
+  proximityState: 'SAFE'
+}
+
 /* GET home page. */
-router.get('/:sensor', function(req, res, next) {
+router.all('/:sensor', function(req, res, next) {
 //  res.json(req.params);
-  var sensorName = req.params.sensor;
-  console.log("Sensor type: " sensorName);
-  res.send(sensorName);
+  var param = req.params.sensor;
+  console.log("Param type: " + param);
+  //res.send(param);
+  var returnTemp = function() {
+    var temp = utility.getTemperature();
+    jsonResponse.value = temp;
+    jsonResponse.timeStamp = new Date().getTime();
+    if(temp <= 15 || temp > 45)
+      jsonResponse.state = 'CRITICAL';
+    if((temp > 15 && temp < 20) || (temp > 35 && temp <= 45))
+      jsonResponse.state = 'WARNING';
+
+  };
+
+  var returnProximity = function() {
+    var temp = utility.getProximity();
+    jsonResponse.value = temp;
+    jsonResponse.timeStamp = new Date().getTime();
+    if(temp <= 15 || temp > 45)
+      jsonResponse.state = 'CRITICAL';
+    if((temp > 15 && temp < 20) || (temp > 35 && temp <= 45))
+      jsonResponse.state = 'WARNING';
+
+  };
+
+  if(param === 'temperature'){
+    returnTemp();
+    res.json(jsonResponse);
+  }
+  else if(param === 'proximity'){
+
+  }
+  else if(param === 'update'){
+
+  }
+  else if(param === 'message'){
+    var message = req.body.message;
+    var phone = req.body.phone;
+    res.send(sendMessage(phone, message));
+  }
+
 });
+
 
 module.exports = router;
